da43b317bb08fe042c7ac4baec416ff8e13d93e1
phijik4189 <phijik4189@gmail.com>
2024-11-20 23:04:58 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,6 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("This is CSE-C");  
-     System.out.println("We are working on jenkins");   
+     System.out.println("We are working on jenkins"); 
+      System.out.println("Webhook");
     }  
 }  
