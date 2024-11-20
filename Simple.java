fd9a0b6a0eaaa8e790f066ed925d05481bf207d6
phijik4189 <phijik4189@gmail.com>
2024-11-20 22:51:17 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("This is CSE-C");  
+     System.out.println("We are working on jenkins");   
     }  
 }  
